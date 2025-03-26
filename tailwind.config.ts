49a2919fccc6674ae041ab7cb41c5af9c0400a80
fambirachimwe <36159141+Fambirachimwe@users.noreply.github.com>
2025-03-26 09:10:19 +0200

    added xx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require('tailwind-scrollbar'), require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
